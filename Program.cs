80745ee4445f664e79004a76d13ab083e7b640a8
Zarpelon <100882885+Zarpelon616@users.noreply.github.com>
2023-08-24 21:02:06 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PjMuriloZarpelon.Data;
+using PjMuriloZarpelon.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<PsicologoDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.
+            GetConnectionString("PjMuriloZarpelonContext")
+    )
+);
+
+builder.Services.AddIdentity<PsicologoDbContext, IdentityRole>(
+    options =>
+    {
+        options.User.RequireUniqueEmail = true;
+        options.Password.RequiredLength = 6;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireDigit = false;
+        options.SignIn.RequireConfirmedPhoneNumber = false; //Processo de fazer login
+        options.Lockout.MaxFailedAccessAttempts = 5;
+        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+    }
+).AddEntityFrameworkStores<PsicologoDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
