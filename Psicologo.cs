5e937cc80f7276e769098526c9f986ac206f2376
Zarpelon <100882885+Zarpelon616@users.noreply.github.com>
2023-08-24 21:02:44 -0300

    Add files via upload

---
+++ Psicologo.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace PjMuriloZarpelon.Models
+{
+    public class Psicologo: IdentityUser
+    {
+
+    }
+
+}
